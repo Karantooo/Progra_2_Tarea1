68e98bed5bc353965637512758cd9c6993bda683
Karantooo <ctomasalvarezn@gmail.com>
2023-10-01 14:55:08 -0300

    Commit inicial para pagos

---
+++ Pago.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.Date;
+
+public class Pago {
+    private float monto;
+    private Date fecha;
+
+    public Pago(float monto, Date fecha) {
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
