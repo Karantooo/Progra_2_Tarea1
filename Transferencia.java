826e0d3291bcb100f24bc555561e3466d26b7046
Karantooo To <ctomasalvarezn@gmail.com>
2023-10-04 10:02:26 -0300

    feat: Documentacion clase Transferencia

--- Transferencia.java
+++ Transferencia.java
@@ -2,16 +2,36 @@ package org.example;
 
 import java.util.Date;
 
+/**
+ * Esta clase esta pensanda para procesar los pagos de transferencia
+ * Esta clase hereda directamente de Pago.
+ * Todo pago hecho por transferencia se asume exacto
+ * @author Carlos Tomás Álvarez Norambuena
+ */
+
 public class Transferencia extends Pago{
     private String banco;
     private String numCuenta;
 
+    /**
+     *
+     * @param monto Monto a transferir.
+     * @param banco Banco desde al que se hizo la transferencia.
+     * @param numCuenta Numero de la cuenta.
+     */
     public Transferencia(float monto, String banco, String numCuenta) {
         super(monto);
         this.banco = banco;
         this.numCuenta = numCuenta;
     }
 
+    /**
+     * Este constructor es igual al anterior con la diferencia de que acepta fecha arbitrarias
+     * @param monto Monto a transferir.
+     * @param fecha Fecha arbitraria en la que se realiza el pago.
+     * @param banco Banco al que se hizo la transferencia.
+     * @param numCuentaNumero de la cuenta.
+     */
     public Transferencia(float monto, Date fecha, String banco, String numCuenta) {
         super(monto, fecha);
         this.banco = banco;
@@ -36,7 +56,7 @@ public class Transferencia extends Pago{
 
     /**
      * Devuelve informaciones varias del objeto.
-     * @return
+     * @return string con informacion de la transferencia
      */
     @Override
     public String toString(){
